5c801673da3f56ad9c74580af58472cd1cbbb61b
wizardcalidad <yusufqoyum01@gmail.com>
2021-06-08 12:30:54 +0100

    new commit as access and secret key are taken away from variables

--- terraform_up_and_running/first.tf
+++ terraform_up_and_running/first.tf
@@ -1,8 +1,8 @@
 provider "aws" {
     region = var.region
     profile = var.profile
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    # access_key = var.aws_access_key
+    # secret_key = var.aws_secret_key
 }
 
 resource "aws_launch_configuration" "first" {
--- terraform_up_and_running/variables.tf
+++ terraform_up_and_running/variables.tf
@@ -6,9 +6,13 @@ variable "profile" {
     description = "AWS credentials profile you want to use"
 }
 
-variable "aws_access_key" {}
+# variable "aws_access_key" {
+#     aws_access_key = "access_key"
+# }
 
-variable "aws_secret_key" {}
+# variable "aws_secret_key" {
+#     aws_secret_key = "secret_key"
+# }
 
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
